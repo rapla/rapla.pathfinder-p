31a6fda7389e5214a851cdb5f1b5a26234bb34d7
Tim Heek <timheek2@gmail.com>
2013-12-06 13:59:38 +0100

    Extending folder structure - adding dummy-classes to enable git indexing

---
+++ src/test/java/com/pathfinder/PathfinderUITest.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+package com.pathfinder;
+
+public class PathfinderUITest {
+	// just a test class to enable structure upload to git
+	// TODO: Delete after adding first 'real' testclass
+}
