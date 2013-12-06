426224fff270fa0a7cf97737d149a62eb26d897a
Alexander Haller <alexanderhaller90@gmail.com>
2013-12-06 19:52:20 +0100

    Added model classes

---
+++ src/main/java/com/pathfinder/model/CoursePojo.java
@@ -0,0 +1,20 @@
+package com.pathfinder.model;
+
+public class CoursePojo {
+	public static final String PROPERTY_XXX = "xxx";
+	
+	private String xxx = "";
+	
+	public CoursePojo()
+	{
+		
+	}
+
+	public String getXxx() {
+		return xxx;
+	}
+
+	public void setXxx(String xxx) {
+		this.xxx = xxx;
+	}
+}
---
+++ src/main/java/com/pathfinder/model/RoomPojo.java
@@ -0,0 +1,20 @@
+package com.pathfinder.model;
+
+public class RoomPojo {
+	public static final String PROPERTY_XXX = "xxx";
+	
+	private String xxx = "";
+	
+	public RoomPojo()
+	{
+		
+	}
+
+	public String getXxx() {
+		return xxx;
+	}
+
+	public void setXxx(String xxx) {
+		this.xxx = xxx;
+	}
+}
