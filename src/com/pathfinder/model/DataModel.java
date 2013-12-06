b6cb0bcae84ae9db89ef8afee625f277a9aed0c3
Igor Nekhoda <nekhoda@googlemail.com>
2013-12-06 19:10:34 +0100

    add JSON Function

---
+++ src/com/pathfinder/model/DataModel.java
@@ -0,0 +1,16 @@
+package com.pathfinder.model;
+
+import java.util.List;
+
+
+public class DataModel {
+	private List<RoomModel> allRooms;
+
+	public List<RoomModel> getAllRooms() {
+		return allRooms;
+	}
+
+	public void setAllRooms(List<RoomModel> allRooms) {
+		this.allRooms = allRooms;
+	}
+}
---
+++ src/com/pathfinder/model/RoomModel.java
@@ -0,0 +1,33 @@
+package com.pathfinder.model;
+
+public class RoomModel {
+
+	String id;
+	String name;
+	String link;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+}
