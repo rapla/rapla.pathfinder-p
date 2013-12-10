685ce8b7a8919182ac373d633b9dc4e32709152f
Alexander Haller <alexanderhaller90@gmail.com>
2013-12-10 11:20:42 +0100

    Revert "Refactor DataLoader Class + add new function to get the details of"
    This reverts commit 2bd1fd1f0fbb7be52ce30145aff61e8615827a23.

--- src/main/interfaces/com/pathfinder/presenter/DataLoaderSpec.java
+++ src/main/interfaces/com/pathfinder/presenter/DataLoaderSpec.java
@@ -1,17 +1,13 @@
 package com.pathfinder.presenter;
 
-import com.pathfinder.model.CourseDetailModel;
-import com.pathfinder.model.PersonDetailModel;
-import com.pathfinder.model.RoomDetailModel;
-
 public interface DataLoaderSpec {
 
-	void loadAllResources();
+	void loadRooms();
 
-	CourseDetailModel getCourseDetail(String id);
+	void loadCourses();
 
-	RoomDetailModel getRoomDetail(String id);
-
-	PersonDetailModel getPersonDetail(String id);
+	void loadPersons();
+	
+	void loadPois();
 
 }
---
+++ src/main/java/com/pathfinder/model/CourseModel.java
@@ -0,0 +1,20 @@
+package com.pathfinder.model;
+
+public class CourseModel {
+	public static final String PROPERTY_XXX = "xxx";
+	
+	private String xxx = "";
+	
+	public CourseModel()
+	{
+		
+	}
+
+	public String getXxx() {
+		return xxx;
+	}
+
+	public void setXxx(String xxx) {
+		this.xxx = xxx;
+	}
+}
