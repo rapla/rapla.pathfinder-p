6ad8de7643fd9a0c4aee13f55d2d1b4ce6c9e343
Myracle <weber.max.1988@googlemail.com>
2014-01-06 19:44:30 +0100

    Language-Control for FreeRooms

--- src/main/java/com/pathfinder/view/components/FreeRoomView.java
+++ src/main/java/com/pathfinder/view/components/FreeRoomView.java
@@ -20,6 +20,17 @@ public class FreeRoomView extends CustomComponent implements FreeRoomViewSpec {
 	private CssLayout cssLayout = new CssLayout();
 	private GridLayout gridLayout;
 
+	private List<String> raumNameList;
+	private List<String> raumLinkList;
+	private List<String> raumIdList;
+	private List<String> startList;
+	private List<String> endList;
+
+	private String room = translator.translate(TranslationKeys.ROOM);
+	private String time = translator.translate(TranslationKeys.TIME);
+	private String floorplan = translator
+			.translate(TranslationKeys.TO_FLOOR_PLAN);
+
 	public FreeRoomView() {
 		buildLayout();
 
@@ -39,24 +50,34 @@ public class FreeRoomView extends CustomComponent implements FreeRoomViewSpec {
 			List<String> raumLinkList, List<String> raumIdList,
 			List<String> startList, List<String> endList) {
 
+		this.raumNameList = raumNameList;
+		this.raumLinkList = raumLinkList;
+		this.raumIdList = raumIdList;
+		this.startList = startList;
+		this.endList = endList;
+
 		// Remove old Grid Layout
 		if (gridLayout != null)
 			cssLayout.removeComponent(gridLayout);
 
-		gridLayout = new GridLayout(2, raumNameList.size());
+		gridLayout = new GridLayout(3, raumNameList.size());
 
 		for (int i = 0; i < raumNameList.size(); i++) {
 
-			Label roomLabel = new Label("Raum " + raumNameList.get(i) + " von "
-					+ startList.get(i) + " bis " + endList.get(i) + " Uhr");
+			Label roomLabel = new Label(room + " " + raumNameList.get(i) + ": ");
 
-			Link roomLink = new Link("zum Raumplan", new ExternalResource(
+			Label roomTime = new Label(startList.get(i) + " - "
+					+ endList.get(i) + " " + time);
+
+			Link roomLink = new Link(floorplan, new ExternalResource(
 					raumLinkList.get(i)));
 
 			roomLabel.setPrimaryStyleName("roomLabel");
+			roomTime.setPrimaryStyleName("roomTime");
 			roomLink.setPrimaryStyleName("roomLink");
 
 			gridLayout.addComponent(roomLabel);
+			gridLayout.addComponent(roomTime);
 			gridLayout.addComponent(roomLink);
 
 			cssLayout.addComponent(gridLayout);
@@ -77,6 +98,16 @@ public class FreeRoomView extends CustomComponent implements FreeRoomViewSpec {
 	public void updateTranslations() {
 		actualFreeRoomsLabel.setCaption(translator
 				.translate(TranslationKeys.CURRENTLY_FREE_ROOMS));
+		translateGridLayout();
+	}
+
+	private void translateGridLayout() {
+		room = translator.translate(TranslationKeys.ROOM);
+		time = translator.translate(TranslationKeys.TIME);
+		floorplan = translator.translate(TranslationKeys.TO_FLOOR_PLAN);
+
+		refreshFreeRooms(raumNameList, raumLinkList, raumIdList, startList,
+				endList);
 	}
 
 	@Override
