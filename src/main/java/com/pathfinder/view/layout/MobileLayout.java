aa51ea44ac357e56672341b040bb70ef6ca21e0e
Alexander Haller <alexanderhaller90@gmail.com>
2014-01-06 16:45:32 +0100

    Added CssLayout to mobile view

--- src/main/java/com/pathfinder/view/layout/MobileLayout.java
+++ src/main/java/com/pathfinder/view/layout/MobileLayout.java
@@ -1,7 +1,9 @@
 package com.pathfinder.view.layout;
 
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.CustomComponent;
 
 public class MobileLayout extends CustomComponent implements MobileLayoutSpec {
+	private final CssLayout layout = new CssLayout();
 
 }
